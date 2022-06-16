85af0ed202dfaf0a0497d78daf345a78394ca0fc
Till Schweneker <t.schweneker@gmail.com>
2022-06-16 15:53:30 +0200

    Bugfixes and style optimization (#5)
    * chore: update dependencies
    
    * multiple fixes and refactorings
    
    - use framer motion to get values
    - allow changing hotspots after initial save
    - update hotspot style for easier targeting
    - use resizeObserver to update hotspot positions when image size changes
    
    * chore: autoformat
    
    * feat: Update hotspot styles
    
    - The hotspot is now semi-opaque and has a dot in the middle, to pinpoint the hotspot position.
    - The cursor is removed while dragging, so it does not distract.
    - The hotspot is moved half its width to the top and left, to accurately display the position.
    
    * fix: set dragElastic to 0 to prevent dragging the hotspot out of bounds
    
    * feat: use framer motion to get x/y values
    
    Instead of parsing the transform of the hotspot, framer motions' useMotionValue hook is used to get the correct value
    
    * chore: convert indentation to spaces
    
    * reafactor: add some types
    
    * change: use DOMRect instead of ImageRef for bounds
    
    The rectangle is tracked by a ResizeObserver and updates the bounds, when the browser window and image resizes. This way the hotspots are always correctly repositioned. The rect is also used to calculate the initial position of the hotspot.
    
    This change also fixes the bug, that hotspot couldn't be changed after initial save
    
    * chore: typecast lodash get to string to fix linter warning
    
    * chore: autoformat
    
    * fix: remove check for missing or non-number x or y
    
    - the checks prevented hotspots wher x or y had the position 0
    - by using a rect for bounds and framer motion to get the position these checks are superflous
    
    * chore: set type of newRow
    
    * fix: update imageUrl when any changes to the image occur
    
    * revert: prepare for better diff
    
    * chore: prettier
    
    * refactor: don't use inline arrow functions, only conditionally apply new styles on drag
    
    Co-authored-by: Till Schweneker <till.schweneker@meltingelements.com>
    Co-authored-by: Robin Pyon <robin.pyon@gmail.com>

--- src/Spot.tsx
+++ src/Spot.tsx
@@ -1,81 +1,77 @@
-import React from 'react'
 import {Box, Card, Text, Tooltip} from '@sanity/ui'
-import {motion} from 'framer-motion'
+import {motion, useMotionValue} from 'framer-motion'
 import get from 'lodash/get'
+import React, {CSSProperties, ReactElement, useEffect} from 'react'
+import {FnHotspotMove, TSpot} from './HotspotArray'
 
-const dragStyle = {
-  width: `1rem`,
-  height: `1rem`,
-  position: `absolute`,
+const dragStyle: CSSProperties = {
+  width: '1rem',
+  height: '1rem',
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  margin: '-0.5rem 0 0 -0.5rem',
+  cursor: 'pointer',
 }
 
-const dotStyle = {
-  ...dragStyle,
-  borderRadius: `50%`,
+const dragStyleWhileDrag: CSSProperties = {
+  cursor: 'none',
+}
+
+const dotStyle: CSSProperties = {
+  width: 'inherit',
+  height: 'inherit',
+  borderRadius: '50%',
+  textAlign: 'center',
+  lineHeight: 1,
+  backgroundColor: 'rgba(255, 255, 255, 0.2)',
+  color: 'black',
   // make sure pointer events only run on the parent
-  pointerEvents: `none`,
+  pointerEvents: 'none',
 }
 
 const round = (num) => Math.round(num * 100) / 100
 
-export default function Spot({spot, bounds = undefined, update, hotspotDescriptionPath = ``, tooltip}) {
-  // x/y are stored as % but need to be displayed as px
-  const [{x, y}, setXY] = React.useState({x: 0, y: 0})
-  const [rect, setRect] = React.useState(bounds?.current ? bounds.current.getBoundingClientRect() : {width: 0, height:0})
+interface IHotspot {
+  spot: TSpot
+  bounds: DOMRectReadOnly
+  update: FnHotspotMove
+  hotspotDescriptionPath?: string
+  tooltip?: ReactElement
+}
+
+export default function Spot({spot, bounds, update, hotspotDescriptionPath, tooltip}: IHotspot) {
   const [isDragging, setIsDragging] = React.useState(false)
+  // x/y are stored as % but need to be converted to px
+  const x = useMotionValue(round(bounds.width * (spot.x / 100)))
+  const y = useMotionValue(round(bounds.height * (spot.y / 100)))
 
-  React.useEffect(() => {
-    const clientRect = bounds?.current?.getBoundingClientRect()
+  /**
+   * update x/y if the bounds change when resizing the window
+   */
+  useEffect(() => {
+    x.set(round(bounds.width * (spot.x / 100)))
+    y.set(round(bounds.height * (spot.y / 100)))
+  }, [bounds])
 
-    if (clientRect) {
-      // So convert % to px once we know the height/width of the image
-      setXY({
-        x: round(clientRect.width * (spot.x / 100)),
-        y: round(clientRect.height * (spot.y / 100)),
-      })
+  const handleDragEnd = React.useCallback(() => {
+    setIsDragging(false)
 
-      if (!rect.width || !rect.height) {
-        setRect(clientRect)
-      }
-    }
-  }, [bounds, bounds.current])
+    // get current values for x/y in px
+    const currentX = x.get()
+    const currentY = y.get()
 
-  const handleDragEnd = React.useCallback(
-    (event) => {
-      setIsDragging(false)
+    // Which we need to convert back to `%` to patch the document
+    const newX = round((currentX * 100) / bounds.width)
+    const newY = round((currentY * 100) / bounds.height)
 
-      // I don't know why, but framer-motion doesn't give you the actual transform values
-      // So we have to regex the `px` values off the inline styles
-      const [currentX, currentY] = event.srcElement.style.transform.split(` `).map((v) => {
-        return v
-          ? v
-              .match(/\(([^)]+)\)/)
-              .pop()
-              .replace(`px`, ``)
-          : null
-      })
+    // Don't go below 0 or above 100
+    const safeX = Math.max(0, Math.min(100, newX))
+    const safeY = Math.max(0, Math.min(100, newY))
 
-      if (!Number(currentX) || !Number(currentY)) {
-        return console.warn(`Missing or non-number X or Y`, {currentX, currentY}, event.srcElement)
-      }
-
-      if (!rect.width || !rect.height) {
-        return console.warn(`Rect width/height not yet set`, {rect}, bounds?.current)
-      }
-
-      // Which we need to convert back to `%` to patch the document
-      const newX = round((currentX * 100) / rect.width)
-      const newY = round((currentY * 100) / rect.height)
-      
-      // Don't go below 0 or above 100
-      const safeX = Math.max(0, Math.min(100, newX))
-      const safeY = Math.max(0, Math.min(100, newY))
-
-      update(spot._key, safeX, safeY)
-    },
-
-    [spot]
-  )
+    update(spot._key, safeX, safeY)
+  }, [spot])
+  const handleDragStart = React.useCallback(() => setIsDragging(true), [])
 
   if (!x || !y) {
     return null
@@ -85,14 +81,17 @@ export default function Spot({spot, bounds = undefined, update, hotspotDescripti
     <Tooltip
       key={spot._key}
       disabled={isDragging}
-      boundaryElement={bounds.current}
       portal
       content={
         tooltip && typeof tooltip === 'function' ? (
           React.createElement(tooltip, {spot})
         ) : (
           <Box padding={2} style={{maxWidth: 200, pointerEvents: `none`}}>
-            <Text textOverflow="ellipsis">{hotspotDescriptionPath ? get(spot, hotspotDescriptionPath) : `${spot.x}% x ${spot.y}%`}</Text>
+            <Text textOverflow="ellipsis">
+              {hotspotDescriptionPath
+                ? (get(spot, hotspotDescriptionPath) as string)
+                : `${spot.x}% x ${spot.y}%`}
+            </Text>
           </Box>
         )
       }
@@ -100,13 +99,20 @@ export default function Spot({spot, bounds = undefined, update, hotspotDescripti
       <motion.div
         drag
         dragConstraints={bounds}
+        dragElastic={0}
         dragMomentum={false}
-        initial={{x, y}}
         onDragEnd={handleDragEnd}
-        onDragStart={() => setIsDragging(true)}
-        style={dragStyle}
+        onDragStart={handleDragStart}
+        style={{
+          ...dragStyle,
+          x,
+          y,
+          ...(isDragging && {...dragStyleWhileDrag}),
+        }}
       >
-        <Card tone="primary" shadow={3} style={dotStyle} />
+        <Card tone="primary" shadow={3} style={dotStyle}>
+          ・
+        </Card>
       </motion.div>
     </Tooltip>
   )
