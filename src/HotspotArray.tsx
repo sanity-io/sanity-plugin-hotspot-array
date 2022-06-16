e219e1804559b0b027b4e2c3f0b66efabe05d9e9
Till Schweneker <till.schweneker@meltingelements.com>
2022-06-16 13:20:43 +0200

    revert: prepare for better diff

--- src/HotspotArray.tsx
+++ src/HotspotArray.tsx
@@ -3,139 +3,142 @@
 // @ts-ignore
 import sanityClient from 'part:@sanity/base/client'
 // @ts-ignore
-import { withDocument } from 'part:@sanity/form-builder'
+import {withDocument} from 'part:@sanity/form-builder'
 
-import { IUseResizeObserverCallback, useDebouncedCallback, useResizeObserver } from '@react-hookz/web'
-import { getImageDimensions } from '@sanity/asset-utils'
-import { FormBuilderInput } from '@sanity/form-builder/lib/FormBuilderInput'
-import { insert, PatchEvent, set, setIfMissing } from '@sanity/form-builder/PatchEvent'
+import React from 'react'
+import {Card, Flex, Stack, Text} from '@sanity/ui'
+import {FormBuilderInput} from '@sanity/form-builder/lib/FormBuilderInput'
+import {randomKey} from '@sanity/util/content'
+import {PatchEvent, setIfMissing, set, insert} from '@sanity/form-builder/PatchEvent'
 import imageUrlBuilder from '@sanity/image-url'
-import { Card, Flex, Stack } from '@sanity/ui'
-import { randomKey } from '@sanity/util/content'
+import {getImageDimensions} from '@sanity/asset-utils'
 import get from 'lodash/get'
-import React, { useCallback, useRef, useState } from 'react'
 
-import Feedback from './Feedback'
 import Spot from './Spot'
 import { useUnsetInputComponent } from './useUnsetInputComponent'
+import Feedback from './Feedback'
 
-const imageStyle = { width: `100%`, height: `auto` }
+const imageStyle = {width: `100%`, height: `auto`}
 
 const VALID_ROOT_PATHS = ['document', 'parent']
 
-export type FnHotspotMove = (key: string, x: number, y: number) => void
-
-export type TSpot = {
-	_key: string
-	_type: `spot`
-	x: number
-	y: number
-} & { [key: string]: unknown }
-
-const HotspotArray = React.forwardRef((props: any, ref) => {
-	const { type, value, onChange, document } = props
-	const { options } = type ?? {}
+const HotspotArray = React.forwardRef((props, ref) => {
+  const {type, value, onChange, document, parent} = props
+  const {options} = type ?? {}
 
   // Attempt prevention of infinite loop in <FormBuilderInput />
-	// Re-renders can still occur if this Component is used again in a nested field
-	const typeWithoutInputComponent = useUnsetInputComponent(type, type?.inputComponent)
-	const imageHotspotPathRoot = React.useMemo(() => (VALID_ROOT_PATHS.includes(options?.imageHotspotPathRoot) ? props[options.imageHotspotPathRoot] : document), [])
+  // Re-renders can still occur if this Component is used again in a nested field
+  const typeWithoutInputComponent = useUnsetInputComponent(type, type?.inputComponent)
 
-	// Finding the image from the imageHotspotPathRoot (defaults to document),
-	// using the path from the hotspot's `options` field
-	const displayImage = React.useMemo(() => {
-		const builder = imageUrlBuilder(sanityClient).dataset(sanityClient.config().dataset)
-		const urlFor = (source) => builder.image(source)
+  // Finding the image from the imageHotspotPathRoot (defaults to document),
+  // using the path from the hotspot's `options` field
+  const displayImage = React.useMemo(() => {
+    const builder = imageUrlBuilder(sanityClient).dataset(sanityClient.config().dataset)
+    const urlFor = (source) => builder.image(source)
 
-		const hotspotImage = get(imageHotspotPathRoot, options?.hotspotImagePath)
+    const imageHotspotPathRoot = VALID_ROOT_PATHS.includes(options?.imageHotspotPathRoot) ? props[options.imageHotspotPathRoot] : document
+    const hotspotImage = get(imageHotspotPathRoot, options?.hotspotImagePath)
 
-		if (hotspotImage?.asset?._ref) {
-			const { aspectRatio } = getImageDimensions(hotspotImage.asset._ref)
-			const width = 600
-			const height = Math.round(width / aspectRatio)
-			const url = urlFor(hotspotImage).width(width).url()
+    if (hotspotImage?.asset?._ref) {
+      const {aspectRatio} = getImageDimensions(hotspotImage.asset._ref)
+      const width = 600
+      const height = Math.round(width / aspectRatio)
+      const url = urlFor(hotspotImage).width(width).url()
 
-			return { width, height, url }
-		}
+      return {width, height, url}
+    }
 
-		return null
-	}, [type, imageHotspotPathRoot])
+    return null
+  }, [type, document])
 
-	const handleHotspotImageClick = React.useCallback((event) => {
-		const { nativeEvent } = event
+  const handleHotspotImageClick = React.useCallback((event) => {
+    const {nativeEvent} = event
 
-		// Calculate the x/y percentage of the click position
-		const x = Number(((nativeEvent.offsetX * 100) / nativeEvent.srcElement.width).toFixed(2))
-		const y = Number(((nativeEvent.offsetY * 100) / nativeEvent.srcElement.height).toFixed(2))
-		const description = `New Hotspot at ${x}% x ${y}%`
+    // Calculate the x/y percentage of the click position
+    const x = Number(((nativeEvent.offsetX * 100) / nativeEvent.srcElement.width).toFixed(2))
+    const y = Number(((nativeEvent.offsetY * 100) / nativeEvent.srcElement.height).toFixed(2))
+    const description = `New Hotspot at ${x}% x ${y}%`
 
-		const newRow: TSpot = {
-			_key: randomKey(12),
-			_type: `spot`,
-			x,
-			y,
-		}
+    const newRow = {
+      _key: randomKey(12),
+      _type: `spot`,
+      x,
+      y,
+    }
 
-		if (options?.hotspotDescriptionPath) {
-			newRow[options.hotspotDescriptionPath] = description
-		}
+    if (options?.hotspotDescriptionPath) {
+      newRow[options.hotspotDescriptionPath] = description
+    }
 
-		onChange(PatchEvent.from(setIfMissing([]), insert([newRow], 'after', [-1])))
-	}, [])
+    onChange(PatchEvent.from(setIfMissing([]), insert([newRow], 'after', [-1])))
+  }, [])
 
-	const handleHotspotMove: FnHotspotMove = useCallback(
-		(key, x, y) => {
-			onChange(
-				PatchEvent.from(
-					// Set the `x` value of this array key item
-					set(x, [{ _key: key }, 'x']),
-					// Set the `y` value of this array key item
-					set(y, [{ _key: key }, 'y'])
-				)
-			)
-		},
-		[value]
-	)
+  const handleHotspotMove = React.useCallback(
+    (key, x, y) => {
+      if (!Number(x) || !Number(y)) {
+        console.warn(`Missing or non-number X or Y`, {x, y})
+        return
+      }
 
-	const hotspotImageRef = useRef<HTMLImageElement | null>(null)
+      onChange(
+        PatchEvent.from(
+          // Set the `x` value of this array key item
+          set(x, [{_key: key}, 'x']),
+          // Set the `y` value of this array key item
+          set(y, [{_key: key}, 'y'])
+        )
+      )
+    },
+    [value]
+  )
 
-	const [imageRect, setImageRect] = useState<DOMRectReadOnly>()
-	const updateImageRectCallback = useDebouncedCallback(((e) => setImageRect(e.contentRect)) as IUseResizeObserverCallback, [setImageRect], 200)
-	useResizeObserver(hotspotImageRef, updateImageRectCallback)
+  const hotspotImageRef = React.useRef(null)
 
-	return (
-		<Stack space={[2, 2, 3]}>
-			{displayImage?.url ? (
-				<div style={{ position: `relative` }}>
-					{imageRect && value?.length > 0 && value.map((spot) => <Spot key={spot._key} spot={spot} bounds={imageRect} update={handleHotspotMove} hotspotDescriptionPath={options?.hotspotDescriptionPath} tooltip={options?.hotspotTooltip} />)}
+  return (
+    <Stack space={[2,2,3]}>
+      {displayImage?.url ? (
+        <div style={{position: `relative`}}>
+          {value?.length > 0 &&
+            value.map((spot) => (
+              <Spot
+                key={spot._key}
+                spot={spot}
+                bounds={hotspotImageRef}
+                update={handleHotspotMove}
+                hotspotDescriptionPath={options?.hotspotDescriptionPath}
+                tooltip={options?.hotspotTooltip}
+              />
+            ))}
 
-					<Card __unstable_checkered shadow={1}>
-						<Flex align="center" justify="center">
-							<img ref={hotspotImageRef} src={displayImage.url} width={displayImage.width} height={displayImage.height} alt="" style={imageStyle} onClick={handleHotspotImageClick} />
-						</Flex>
-					</Card>
-				</div>
-			) : (
-				<Feedback>
-					{type?.options?.hotspotImagePath ? (
-						<>
-							No Hotspot image found at path <code>{type?.options?.hotspotImagePath}</code>
-						</>
-					) : (
-						<>
-							Define a path in this field using to the image field in this document at <code>options.hotspotImagePath</code>
-						</>
-					)}
-				</Feedback>
-			)}
-			{type?.options?.imageHotspotPathRoot && !VALID_ROOT_PATHS.includes(type.options.imageHotspotPathRoot) && (
-				<Feedback>
-					The supplied imageHotspotPathRoot "{type.options.imageHotspotPathRoot}" is not valid, falling back to "document". Available values are "{VALID_ROOT_PATHS.join(', ')}".
-				</Feedback>
-			)}
-			<FormBuilderInput {...props} type={typeWithoutInputComponent} ref={ref} />
-		</Stack>
-	)
+          <Card __unstable_checkered shadow={1}>
+            <Flex align="center" justify="center">
+              <img
+                ref={hotspotImageRef}
+                src={displayImage.url}
+                width={displayImage.width}
+                height={displayImage.height}
+                alt=""
+                style={imageStyle}
+                onClick={handleHotspotImageClick}
+                />
+            </Flex>
+          </Card>
+        </div>
+      ) : (
+        <Feedback>
+            {type?.options?.hotspotImagePath
+              ? <>No Hotspot image found at path <code>{type?.options?.hotspotImagePath}</code></>
+              : <>Define a path in this field using to the image field in this document at <code>options.hotspotImagePath</code></>
+            }
+          </Feedback>
+      )}
+        {type?.options?.imageHotspotPathRoot && !VALID_ROOT_PATHS.includes(type.options.imageHotspotPathRoot) &&
+        <Feedback>
+            The supplied imageHotspotPathRoot "{type.options.imageHotspotPathRoot}" is not valid, falling back to "document". Available values are "{VALID_ROOT_PATHS.join(', ')}".
+        </Feedback>}
+      <FormBuilderInput {...props} type={typeWithoutInputComponent} ref={ref} />
+    </Stack>
+  )
 })
 
 export default withDocument(HotspotArray)
--- src/Spot.tsx
+++ src/Spot.tsx
@@ -1,106 +1,113 @@
-import { Box, Card, Text, Tooltip } from '@sanity/ui'
-import { motion, useMotionValue } from 'framer-motion'
-import get from 'lodash/get'
-import { createElement, CSSProperties, ReactElement, useCallback, useEffect, useState } from 'react'
-import { FnHotspotMove, TSpot } from './HotspotArray'
 import React from 'react'
+import {Box, Card, Text, Tooltip} from '@sanity/ui'
+import {motion} from 'framer-motion'
+import get from 'lodash/get'
 
-const dragStyle: CSSProperties = {
-	width: '1rem',
-	height: '1rem',
-	position: 'absolute',
-	top: 0,
-	left: 0,
-	margin: '-0.5rem 0 0 -0.5rem',
-	cursor: 'pointer',
+const dragStyle = {
+  width: `1rem`,
+  height: `1rem`,
+  position: `absolute`,
 }
 
-const dragStyleWhileDrag: CSSProperties = {
-	cursor: 'none',
-}
-
-const dotStyle: CSSProperties = {
-	width: 'inherit',
-	height: 'inherit',
-	borderRadius: '50%',
-	textAlign: 'center',
-	lineHeight: 1,
-	backgroundColor: 'rgba(255, 255, 255, 0.2)',
-	color: 'black',
-	// make sure pointer events only run on the parent
-	pointerEvents: 'none',
+const dotStyle = {
+  ...dragStyle,
+  borderRadius: `50%`,
+  // make sure pointer events only run on the parent
+  pointerEvents: `none`,
 }
 
 const round = (num) => Math.round(num * 100) / 100
 
-interface IHotspot {
-	spot: TSpot
-	bounds: DOMRectReadOnly
-	update: FnHotspotMove
-	hotspotDescriptionPath?: string
-	tooltip?: ReactElement
-}
+export default function Spot({spot, bounds = undefined, update, hotspotDescriptionPath = ``, tooltip}) {
+  // x/y are stored as % but need to be displayed as px
+  const [{x, y}, setXY] = React.useState({x: 0, y: 0})
+  const [rect, setRect] = React.useState(bounds?.current ? bounds.current.getBoundingClientRect() : {width: 0, height:0})
+  const [isDragging, setIsDragging] = React.useState(false)
 
-export default function Spot({ spot, bounds, update, hotspotDescriptionPath, tooltip }: IHotspot) {
-	const [isDragging, setIsDragging] = useState(false)
-	// x/y are stored as % but need to be converted to px
-	const x = useMotionValue(round(bounds.width * (spot.x / 100)))
-	const y = useMotionValue(round(bounds.height * (spot.y / 100)))
+  React.useEffect(() => {
+    const clientRect = bounds?.current?.getBoundingClientRect()
 
-	/**
-	 * update x/y if the bounds change when resizing the window
-	 */
-	useEffect(() => {
-		x.set(round(bounds.width * (spot.x / 100)))
-		y.set(round(bounds.height * (spot.y / 100)))
-	}, [bounds])
+    if (clientRect) {
+      // So convert % to px once we know the height/width of the image
+      setXY({
+        x: round(clientRect.width * (spot.x / 100)),
+        y: round(clientRect.height * (spot.y / 100)),
+      })
 
-	const handleDragEnd = useCallback(() => {
-		setIsDragging(false)
+      if (!rect.width || !rect.height) {
+        setRect(clientRect)
+      }
+    }
+  }, [bounds, bounds.current])
 
-		// get current values for x/y in px
-		const currentX = x.get()
-		const currentY = y.get()
+  const handleDragEnd = React.useCallback(
+    (event) => {
+      setIsDragging(false)
 
-		if (!bounds.width || !bounds.height) {
-			return console.warn(`Rect width/height not yet set`, { bounds })
-		}
+      // I don't know why, but framer-motion doesn't give you the actual transform values
+      // So we have to regex the `px` values off the inline styles
+      const [currentX, currentY] = event.srcElement.style.transform.split(` `).map((v) => {
+        return v
+          ? v
+              .match(/\(([^)]+)\)/)
+              .pop()
+              .replace(`px`, ``)
+          : null
+      })
 
-		// convert back to `%` to patch the document
-		const newX = round((currentX * 100) / bounds.width)
-		const newY = round((currentY * 100) / bounds.height)
+      if (!Number(currentX) || !Number(currentY)) {
+        return console.warn(`Missing or non-number X or Y`, {currentX, currentY}, event.srcElement)
+      }
 
-		// don't go below 0 or above 100
-		const safeX = Math.max(0, Math.min(100, newX))
-		const safeY = Math.max(0, Math.min(100, newY))
+      if (!rect.width || !rect.height) {
+        return console.warn(`Rect width/height not yet set`, {rect}, bounds?.current)
+      }
 
-		update(spot._key, safeX, safeY)
-	}, [spot])
+      // Which we need to convert back to `%` to patch the document
+      const newX = round((currentX * 100) / rect.width)
+      const newY = round((currentY * 100) / rect.height)
+      
+      // Don't go below 0 or above 100
+      const safeX = Math.max(0, Math.min(100, newX))
+      const safeY = Math.max(0, Math.min(100, newY))
 
-	if (!x || !y) {
-		return null
-	}
+      update(spot._key, safeX, safeY)
+    },
 
-	return (
-		<Tooltip
-			key={spot._key}
-			disabled={isDragging}
-			portal
-			content={
-				tooltip && typeof tooltip === 'function' ? (
-					createElement(tooltip, { spot })
-				) : (
-					<Box padding={2} style={{ maxWidth: 200, pointerEvents: `none` }}>
-						<Text textOverflow="ellipsis">{hotspotDescriptionPath ? get(spot, hotspotDescriptionPath) as string : `${spot.x}% x ${spot.y}%`}</Text>
-					</Box>
-				)
-			}
-		>
-			<motion.div drag dragConstraints={bounds} dragElastic={0} dragMomentum={false} onDragEnd={handleDragEnd} onDragStart={() => setIsDragging(true)} style={isDragging ? { ...dragStyle, ...dragStyleWhileDrag, x, y } : { ...dragStyle, x, y }}>
-				<Card tone="primary" shadow={3} style={dotStyle}>
-					・
-				</Card>
-			</motion.div>
-		</Tooltip>
-	)
+    [spot]
+  )
+
+  if (!x || !y) {
+    return null
+  }
+
+  return (
+    <Tooltip
+      key={spot._key}
+      disabled={isDragging}
+      boundaryElement={bounds.current}
+      portal
+      content={
+        tooltip && typeof tooltip === 'function' ? (
+          React.createElement(tooltip, {spot})
+        ) : (
+          <Box padding={2} style={{maxWidth: 200, pointerEvents: `none`}}>
+            <Text textOverflow="ellipsis">{hotspotDescriptionPath ? get(spot, hotspotDescriptionPath) : `${spot.x}% x ${spot.y}%`}</Text>
+          </Box>
+        )
+      }
+    >
+      <motion.div
+        drag
+        dragConstraints={bounds}
+        dragMomentum={false}
+        initial={{x, y}}
+        onDragEnd={handleDragEnd}
+        onDragStart={() => setIsDragging(true)}
+        style={dragStyle}
+      >
+        <Card tone="primary" shadow={3} style={dotStyle} />
+      </motion.div>
+    </Tooltip>
+  )
 }
